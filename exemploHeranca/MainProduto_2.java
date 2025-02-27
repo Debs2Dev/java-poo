b7f7209390deea29dc9ca6fbad9deb07e1811dec
Débora <168920480+Debs2Dev@users.noreply.github.com>
2025-02-27 00:40:00 +0000

    heranca2

--- exemploHeranca/MainProduto_2.java
+++ exemploHeranca/MainProduto_2.java
@@ -2,8 +2,8 @@ package exemploHeranca;
 
 public class MainProduto_2 {
     public static void main(String[] args) {
-        Processador processador1= new Processador("AMD", "Ryzen", 3.5);
-        System.out.println(processador1);
+        //Processador processador1= new Processador("AMD", "Ryzen", 3.5);
+        //System.out.println(processador1);
     }
 
 }
